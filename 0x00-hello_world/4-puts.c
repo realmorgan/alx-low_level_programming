f5f8b27d9efc7447624547b0106dadd038517a9a
realmorgan <prcsmorgan@gmail.com>
2023-06-19 13:08:28 -0700

    using the function puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -it prints out ouput using put
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
